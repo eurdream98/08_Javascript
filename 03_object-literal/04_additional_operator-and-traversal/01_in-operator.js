a83cf3fcaf2c3726a62d96828f57dd0d0ea596be
eurdream98 <eurdream98@naver.com>
2024-02-19 18:05:46 +0900

    2024_02_19 Javascript_리터럴객체_함수

---
+++ 03_object-literal/04_additional_operator-and-traversal/01_in-operator.js
@@ -0,0 +1,14 @@
+// in 연산자 : 프로퍼티 존재 여부 확인
+var student ={
+    name : '유관순',
+    age : 16,
+    test : undefined
+};
+
+console.log(student.name === undefined);    //false - 해당 프로퍼티가 객체 안에 존재
+console.log(student.height === undefined);  //true - 해당 프로퍼티가 객체안에 존재하지 않음
+console.log(student.test===undefined); //true - 프로퍼티는 존재하지만 값이 undefined인 상황
+
+console.log("name" in student);
+console.log("age" in student);
+console.log("test" in student);
